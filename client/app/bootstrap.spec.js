99a785dda40058664168555c2bc905f5063894f3
m.sakamaki <m.sakamaki@gxp.co.jp>
2015-02-03 21:35:09 +0900

    add gulp test

---
+++ client/app/bootstrap.spec.js
@@ -0,0 +1,12 @@
+'use strict';
+
+describe('bootstrap.js test', function () {
+
+  it('test one...', function () {
+     expect(1).to.be.ok;
+  });
+
+  it('test tow...', function () {
+     expect(1).to.be.ok;
+  });
+});
--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,17 @@ require('require-dir')('./gulp/tasks');
 
 // DEVELOPMENT TASKS
 //================================================
+var gulp = require('gulp');
+var karma = require('karma').server;
+
+// express(mock)
+// Tests
+gulp.task('test', function(done) {
+  karma.start({
+    configFile: __dirname + '/karma.conf.js'
+  }, done);
+});
+
 // serve task
 gulp.task('serve', [
     'browser-sync',
