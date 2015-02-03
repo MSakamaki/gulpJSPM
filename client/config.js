ab01dc8f6a886e1053a7159462ebab41e404eb7b
m.sakamaki <m.sakamaki@gxp.co.jp>
2015-02-03 18:42:15 +0900

    jspm install jquery

--- client/config.js
+++ client/config.js
@@ -1,7 +1,14 @@
 System.config({
   "paths": {
     "*": "*.js",
-    "gulpjspm/*": "app/*.js"
+    "gulpjspm/*": "lib/*.js",
+    "github:*": "jspm_packages/github/*.js"
+  }
+});
+
+System.config({
+  "map": {
+    "jquery": "github:components/jquery@2.1.3"
   }
 });
 
