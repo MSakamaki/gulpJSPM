cce6778127d5c7054d13a96c328642b844d49e85
m.sakamaki <m.sakamaki@gxp.co.jp>
2015-02-03 18:55:23 +0900

    add action

--- client/app/bootstrap.js
+++ client/app/bootstrap.js
@@ -2,13 +2,18 @@ import $ from 'jquery';
 
 export function bootstrap() {
   // bootstrap code here
-  $.ajax({
-      url : "http://localhost:3000/api/test",
-      dataType : 'json',  
-      success : (data) => {
-        document.getElementById('app').innerText += ' server read ['+data.message+'] '
-      }, error :(err) => {
-        console.log("Data err: ", err);
-      }
+
+  $('#app').append('<button id="addbtn">server connect</button>')
+  $('#app').append('<ul id="lists"><li>hoge</li></ul>')
+  $('#addbtn').click(()=>{
+    $.ajax({
+        url : "http://localhost:3000/api/test",
+        dataType : 'json',
+        success : (data) => {
+          $('#lists').append('<li>'+data.message+'</li>')
+        }, error :(err) => {
+          console.log("Data err: ", err);
+        }
+    });
   });
 }
