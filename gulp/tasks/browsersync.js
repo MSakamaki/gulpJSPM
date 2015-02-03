2316261fc32d9dd4276b21ed153c9df7b0d2c045
m.sakamaki <m.sakamaki@gxp.co.jp>
2015-02-03 18:33:07 +0900

    split gulp task

---
+++ gulp/tasks/browsersync.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+
+gulp.task('browser-sync', function() {
+  browserSync.init([
+    './client/app/**/*.js',
+    './client/**/*.html'
+  ],
+  {
+    notify: true,
+    server: {
+      baseDir: ['./client']
+    },
+    port: 9000,
+    // firefox, google chrome, 
+    browser: ['google chrome'],
+    tunnel: true
+  });
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,18 @@
 'use strict';
 
 var gulp = require('gulp');
-var plugins = require('gulp-load-plugins')();
-var browserSync = require('browser-sync');
-var server = require('gulp-express');
+
+require('gulp-load-plugins')();
+require('require-dir')('./gulp/tasks');
 
 // DEVELOPMENT TASKS
 //================================================
-
-// BrowserSync Server
-gulp.task('browser-sync', function() {
-  browserSync.init([
-    './client/app/**/*.js',
-    './client/**/*.html'
-  ],
-  {
-    notify: true,
-    server: {
-      baseDir: ['./client']
-    },
-    port: 9000,
-    // firefox, google chrome, 
-    browser: ['google chrome'],
-    tunnel: true
-  });
-});
-
 // serve task
 gulp.task('serve', [
     'browser-sync',
     'express'
-  ] , function(cb) {});
+  ]);
 
 // Default
 gulp.task('default', ['serve']);
 
-// express(mock)
-gulp.task('express', function(){
-  server.run({
-    file: './server/app.js'
-  });
-});
